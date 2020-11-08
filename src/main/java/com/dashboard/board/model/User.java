68d0473c838eebe892da0e060a4a8c301e0b4fed
Steinny <charitymbisi@gmail.com>
2020-11-08 16:36:02 +0300

    user

---
+++ src/main/java/com/dashboard/board/model/User.java
@@ -0,0 +1,28 @@
+package com.dashboard.board.model;
+
+import com.sun.istack.NotNull;
+import lombok.Data;
+import org.hibernate.validator.constraints.NotEmpty;
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table("APP_USER")
+@Data
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(name="NAME", nullable=false)
+    private String name;
+
+    @Column(name="AGE", nullable=false)
+    private Integer age;
+
+    @Column(name="SALARY", nullable=false)
+    private double salary;
+
+
+}
