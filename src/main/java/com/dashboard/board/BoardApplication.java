0e9057de2751d2fa27ac9b373cd2e978432c64fc
Steinny <charitymbisi@gmail.com>
2020-11-08 16:17:54 +0300

    jpa

--- src/main/java/com/dashboard/board/BoardApplication.java
+++ src/main/java/com/dashboard/board/BoardApplication.java
@@ -1,9 +1,12 @@
 package com.dashboard.board;
 
+import com.dashboard.board.configuration.JpaConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.dashboard.board")
+@Import(JpaConfiguration.class)
 public class BoardApplication {
 
 	public static void main(String[] args) {
