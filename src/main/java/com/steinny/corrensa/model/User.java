b8cc6a75775acc7ca4df2147e86424c971fef234
Steinny <charitymbisi@gmail.com>
2020-11-15 22:34:15 +0300

    user model

--- src/main/java/com/steinny/corrensa/model/User.java
+++ src/main/java/com/steinny/corrensa/model/User.java
@@ -1,12 +1,37 @@
 package com.steinny.corrensa.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+import java.time.Instant;
 
 import static javax.persistence.GenerationType.IDENTITY;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class User {
     @Id
     @GeneratedValue(strategy = IDENTITY)
+    private Long userId;
+    @NotBlank(message = "User name is required")
+    private String userName;
+    @NotBlank(message = "Password is required")
+    private String password;
+    @Email
+    @NotEmpty(message = "Email is required")
+    private String email;
+    private Instant created;
+    private boolean enabled;
     
 }
