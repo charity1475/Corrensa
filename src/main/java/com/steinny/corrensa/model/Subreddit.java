0a44c5fa4417a2f43fde03d921e87c9003e8d30e
Steinny <charitymbisi@gmail.com>
2020-11-15 22:29:08 +0300

    subreddit

--- src/main/java/com/steinny/corrensa/model/Subreddit.java
+++ src/main/java/com/steinny/corrensa/model/Subreddit.java
@@ -4,11 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+import java.time.Instant;
+import java.util.List;
+
+import static javax.persistence.FetchType.LAZY;
 import static javax.persistence.GenerationType.IDENTITY;
 
 @Data
@@ -21,5 +24,13 @@ public class Subreddit {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
-    
+    @NotBlank(message = "Community name is required")
+    private String name;
+    @NotBlank(message = "Description is required")
+    private String description;
+    @OneToMany(fetch = LAZY)
+    private List<Post> posts;
+    private Instant createdDate;
+    @ManyToOne(fetch = LAZY)
+    private User user;
 }
--- src/main/java/com/steinny/corrensa/model/User.java
+++ src/main/java/com/steinny/corrensa/model/User.java
@@ -1,4 +1,12 @@
 package com.steinny.corrensa.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
 public class User {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    
 }
