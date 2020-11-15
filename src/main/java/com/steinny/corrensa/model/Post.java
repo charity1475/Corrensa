3d7daf818366b6f0526e20a55e3f4302b7712ff8
Steinny <charitymbisi@gmail.com>
2020-11-15 22:22:28 +0300

    post

--- src/main/java/com/steinny/corrensa/model/Post.java
+++ src/main/java/com/steinny/corrensa/model/Post.java
@@ -4,8 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Entity;
+import org.springframework.lang.Nullable;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.Instant;
+import static javax.persistence.GenerationType.IDENTITY;
 
 @Data
 @Entity
@@ -13,5 +16,23 @@ import javax.persistence.Entity;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Post {
-    
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long postId;
+    @NotBlank(message = "Post Name cannot be empty or Null")
+    private String postName;
+    @Nullable
+    private String url;
+    @Nullable
+    @Lob
+    private String description;
+    private Integer voteCount = 0;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+    private Instant createdDate;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id", referencedColumnName = "id")
+    private Subreddit subreddit;
 }
--- src/main/java/com/steinny/corrensa/model/Subreddit.java
+++ src/main/java/com/steinny/corrensa/model/Subreddit.java
@@ -1,4 +1,25 @@
 package com.steinny.corrensa.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
 public class Subreddit {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    
 }
