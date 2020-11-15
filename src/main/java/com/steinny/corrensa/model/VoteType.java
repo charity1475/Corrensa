06f9eb9b20df5ad670c571b8b3ed60fa17ff2404
Steinny <charitymbisi@gmail.com>
2020-11-15 23:12:56 +0300

    comments model

--- src/main/java/com/steinny/corrensa/model/VoteType.java
+++ src/main/java/com/steinny/corrensa/model/VoteType.java
@@ -9,13 +9,14 @@ public enum VoteType {
     VoteType(int direction){
 
     }
-    public static VoteType lookup(Integer direction){
+    /*public static VoteType lookup(Integer direction){
         return Arrays.stream(VoteType.values())
                 .filter(value -> value.getDirection().equals(direction))
                 .findAny()
-                .orElseThrow(() -> new SpringRedditException("Vote not found"));
-    }
+               // .orElseThrow(() -> new SpringRedditException("Vote not found"));
+    }*/
 
     private Integer getDirection() {
+        return direction;
     }
 }
