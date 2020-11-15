d4cc6dc395638ab2186fbf5bbe3cf7b417a444f4
Steinny <charitymbisi@gmail.com>
2020-11-16 00:13:49 +0300

    repo

--- src/main/java/com/steinny/corrensa/repository/PostRepository.java
+++ src/main/java/com/steinny/corrensa/repository/PostRepository.java
@@ -1,4 +1,7 @@
 package com.steinny.corrensa.repository;
 
-public interface PostRepository {
+import com.steinny.corrensa.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
