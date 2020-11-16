49e6964f4e6d61d8eae7078fd901dec4668127ee
Steinny <charitymbisi@gmail.com>
2020-11-17 00:31:46 +0300

    notification email

--- src/main/java/com/steinny/corrensa/model/NotificationEmail.java
+++ src/main/java/com/steinny/corrensa/model/NotificationEmail.java
@@ -1,4 +1,14 @@
 package com.steinny.corrensa.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NotificationEmail {
+    private String subject;
+    private String recipient;
+    private String body;
 }
---
+++ src/main/java/com/steinny/corrensa/services/MailService.java
@@ -0,0 +1,24 @@
+package com.steinny.corrensa.services;
+
+import com.steinny.corrensa.model.NotificationEmail;
+import lombok.AllArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class MailService {
+
+    private final JavaMailSender mailSender;
+    private final MailContentBuilder mailContentBuilder;
+
+    public void sendMail(NotificationEmail notificationEmail){
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom("info@corrensa.com");
+            messageHelper.setTo(notificationEmail.getRecipient());
+        }
+    }
+}
