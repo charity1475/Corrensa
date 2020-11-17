7f92c06bf864e5d73b1228f1d426b4b770bae110
Steinny <charitymbisi@gmail.com>
2020-11-17 23:04:50 +0300

    impl

--- src/main/java/com/steinny/corrensa/services/UserDetailsServiceImpl.java
+++ src/main/java/com/steinny/corrensa/services/UserDetailsServiceImpl.java
@@ -1,4 +1,28 @@
 package com.steinny.corrensa.services;
 
-public class UserDetailsServiceImpl {
+import com.steinny.corrensa.model.User;
+import com.steinny.corrensa.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        Optional<User> userOptional = userRepository.findByUserName(username);
+        User user = new User();
+        return null;
+    }
 }
