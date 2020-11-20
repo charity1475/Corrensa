f7b6f5899b858c241d17f04751fe5e0bdbe254ad
Steinny <charitymbisi@gmail.com>
2020-11-20 22:34:29 +0300

    auth service

--- src/main/java/com/steinny/corrensa/controller/AuthController.java
+++ src/main/java/com/steinny/corrensa/controller/AuthController.java
@@ -5,10 +5,7 @@ import com.steinny.corrensa.services.AuthService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -21,5 +18,9 @@ public class AuthController {
     public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest){
         authService.signup(registerRequest);
         return new ResponseEntity<>("User Registration succeeded", HttpStatus.OK);
+    }
+    @GetMapping("accountVerificationToken/{token}")
+    public ResponseEntity<String> verifyAccount(@PathVariable String token){
+        authService.verifyAccount(token);
     }
 }
