77a11d588d5b9d4d08e4ce36a37450a88e6b4ea1
Steinny <charitymbisi@gmail.com>
2020-11-24 22:51:36 +0300

    refresh token

--- src/main/java/com/steinny/corrensa/services/RefreshTokenService.java
+++ src/main/java/com/steinny/corrensa/services/RefreshTokenService.java
@@ -1,4 +1,24 @@
 package com.steinny.corrensa.services;
 
+
+import com.steinny.corrensa.model.RefreshToken;
+import com.steinny.corrensa.repository.RefreshTokenRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+@Transactional
 public class RefreshTokenService {
+    private final RefreshTokenRepository refreshTokenRepository;
+    public RefreshToken generateRefreshToken(){
+        RefreshToken refreshToken = new RefreshToken();
+        refreshToken.setToken(UUID.randomUUID().toString());
+        refreshToken.setCreatedDate(Instant.now());
+        return refreshTokenRepository.save(refreshToken);
+    }
 }
