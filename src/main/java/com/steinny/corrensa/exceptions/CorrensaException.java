3a5be7ab0c5b805e3357ff77b82dff852eb30254
Steinny <charitymbisi@gmail.com>
2020-11-24 21:51:46 +0300

    blending

--- src/main/java/com/steinny/corrensa/exceptions/CorrensaException.java
+++ src/main/java/com/steinny/corrensa/exceptions/CorrensaException.java
@@ -1,10 +1,17 @@
 package com.steinny.corrensa.exceptions;
 
+import lombok.AllArgsConstructor;
+
 import java.security.GeneralSecurityException;
+@AllArgsConstructor
 
 public class CorrensaException extends RuntimeException {
 
     public CorrensaException(String exMessage, GeneralSecurityException e) {
         super(exMessage);
+    }
+
+    public CorrensaException(String s) {
+
     }
 }
